971556ad5eb23802c4d3c9fef188c6168909e4d5
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-05-09 23:36:32 -0300

    Criar MedicoModel

---
+++ src/main/java/br/com/smartmed/consultas/model/MedicoModel.java
@@ -0,0 +1,58 @@
+package br.com.smartmed.consultas.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "medico")
+
+public class MedicoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nome", nullable = false, length = 255)
+    @Null(message = "O nome do médico não pode ser nulo")
+    @NotBlank(message = "O nome do médico não pode ficar em branco")
+    private String nome;
+
+    @Column(name = "crm", nullable = false, length = 8)
+    @Null(message = "O CRM do médico não pode ser nulo")
+    @NotBlank(message = "O CRM do médico não pode ficar em branco")
+    private String crm;
+
+    @Column(name = "telefone", nullable = false, length = 11)
+    @Null(message = "O telefone do médico não pode ser nulo")
+    @NotBlank(message = "O telefone do médico não pode ficar em branco")
+    private String telefone;
+
+    @Column(name = "email", nullable = false, length = 64)
+    @Null(message = "O email do médico não pode ser nulo")
+    @NotBlank(message = "O email do médico não pode ficar em branco")
+    private String email;
+
+    @Column(name = "valorConsultaReferencia", nullable = false)
+    @Null(message = "O valor da consulta do médico não pode ser nulo")
+    @NotBlank(message = "O valor da consulta do médico não pode ficar em branco")
+    private float valorConsultaReferencia;
+
+    @Column(name = "ativo", nullable = false)
+    @Null(message = "O status do médico não pode ser nulo")
+    @NotBlank(message = "O status do médico não pode ficar em branco")
+    private boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "especialidadeID", nullable = false)
+    private EspecialidadeModel especialidade;
+}
