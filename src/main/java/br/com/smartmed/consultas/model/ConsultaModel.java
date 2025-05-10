b28b023394411c51b734aeee4659f588dd0ee0ad
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-05-10 01:02:10 -0300

    Criação de Consulta

---
+++ src/main/java/br/com/smartmed/consultas/model/ConsultaModel.java
@@ -0,0 +1,40 @@
+package br.com.smartmed.consultas.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "consulta")
+public class ConsultaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "dataHoraConsulta", nullable = false)
+    @NotNull(message = "Data e hora da consulta não podem ser nulas")
+    private LocalDate dataHoraConsulta;
+
+    @Column(name = "status", nullable = false, length = 16)
+    @NotNull(message = "Status da consulta não pode ser nulo")
+    @NotBlank(message = "Status da consulta não pode ficar em branco")
+    private String status;
+
+    @Column(name = "valor", nullable = true) // Pode ser consulta com convênio
+    private float valor;
+
+    @Column(name = "observacoes", nullable = true)
+    private String observacoes;
+
+
+
+}
