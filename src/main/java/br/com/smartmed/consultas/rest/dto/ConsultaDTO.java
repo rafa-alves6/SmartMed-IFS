d1bed7d95fd919de4c56d834daf9cf41c303c531
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-06-04 15:26:20 -0300

    adicionar modelmapper

---
+++ src/main/java/br/com/smartmed/consultas/rest/dto/ConsultaDTO.java
@@ -0,0 +1,10 @@
+package br.com.smartmed.consultas.dto;
+
+public class ConsultaDTO {
+    private Long id;
+    private LocalDate dataHoraConsulta;
+    private String status;
+    private float valor;
+    private String observacoes;
+
+}
---
+++ src/main/java/br/com/smartmed/consultas/rest/dto/FormaPagamentoDTO.java
@@ -0,0 +1,4 @@
+package br.com.smartmed.consultas.dto;
+
+public class FormaPagamentoDTO {
+}
