74415b3b224c0e298181fc27e5ad500be3b15b6d
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-06-12 20:21:21 -0300

    adicionar controllers de paciente e especialidade
    o controller de especialidade usa 'noContent.build()' no método return
    para retornar mensagem de status '204 No Content', ao invés de não
    retornar nada em requisições DELETE

--- src/main/java/br/com/smartmed/consultas/rest/controller/EspecialidadeController.java
+++ src/main/java/br/com/smartmed/consultas/rest/controller/EspecialidadeController.java
@@ -1,4 +1,53 @@
 package br.com.smartmed.consultas.rest.controller;
 
+import br.com.smartmed.consultas.model.EspecialidadeModel;
+import br.com.smartmed.consultas.rest.dto.EspecialidadeDTO;
+import br.com.smartmed.consultas.service.EspecialidadeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("api/especialidade")
 public class EspecialidadeController {
-}
+    @Autowired
+    private EspecialidadeService especialidadeService;
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<EspecialidadeDTO> obterPorId(@PathVariable Integer id) {
+        EspecialidadeDTO especialidadeDTO = especialidadeService.buscarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(especialidadeDTO);
+    }
+
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<EspecialidadeDTO> obterPorNome(@PathVariable String nome) {
+        EspecialidadeDTO especialidadeDTO = especialidadeService.buscarPorNome(nome);
+        return ResponseEntity.status(HttpStatus.OK).body(especialidadeDTO);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<EspecialidadeDTO>> obterTodos() {
+        List<EspecialidadeDTO> especialidades = especialidadeService.buscarTodos();
+        return ResponseEntity.ok(especialidades);
+    }
+
+    @PostMapping()
+    public ResponseEntity<EspecialidadeDTO> salvar(@Valid @RequestBody EspecialidadeModel novaEspecialidade) {
+        EspecialidadeDTO novaEspecialidadeDTO = especialidadeService.salvar(novaEspecialidade);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaEspecialidadeDTO);
+    }
+
+    @PutMapping()
+    public ResponseEntity<EspecialidadeDTO> atualizar(@Valid @RequestBody EspecialidadeModel especialidadeExistente) {
+        EspecialidadeDTO especialidadeExistenteDTO = especialidadeService.atualizar(especialidadeExistente);
+        return ResponseEntity.status(HttpStatus.OK).body(especialidadeExistenteDTO);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<Void> deletar(@Valid @RequestBody EspecialidadeModel especialidadeExistente) {
+        especialidadeService.deletar(especialidadeExistente);
+        return ResponseEntity.noContent().build();
+    }
--- src/main/java/br/com/smartmed/consultas/rest/controller/PacienteController.java
+++ src/main/java/br/com/smartmed/consultas/rest/controller/PacienteController.java
@@ -1,4 +1,91 @@
 package br.com.smartmed.consultas.rest.controller;
 
+import br.com.smartmed.consultas.model.PacienteModel;
+import br.com.smartmed.consultas.rest.dto.PacienteDTO;
+import br.com.smartmed.consultas.service.PacienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Controlador responsável por gerenciar as operações relacionadas aos pacientes.
+ */
+@RestController
+@RequestMapping("api/paciente")
 public class PacienteController {
+
+    /**
+     * Instância do serviço de pacientes, responsável por encapsular a lógica de negócios
+     * e intermediar as operações entre o controlador e o repositório.
+     */
+    @Autowired
+    private PacienteService pacienteService;
+
+    /**
+     * Obtém um paciente pelo ID.
+     * Link: http://localhost:8080/api/paciente/?
+     *
+     * @param id ID do paciente.
+     * @return pacienteDTO representando o paciente encontrado.
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteDTO> obterPorId(@PathVariable int id) {
+        PacienteDTO pacienteDTO = pacienteService.obterPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteDTO);
+    }
+
+    /**
+     * Obtém a lista de todos os pacientes cadastrados.
+     * Link: <a href="http://localhost:8080/api/paciente">...</a>
+     *
+     * @return Lista de pacienteDTO representando os pacientes cadastrados.
+     */
+    @GetMapping()
+    public ResponseEntity<List<PacienteDTO>> obterTodos() {
+        List<PacienteDTO> pacienteDTOList = pacienteService.obterTodos();
+        return ResponseEntity.ok(pacienteDTOList);
+    }
+
+    /**
+     * Salva um novo paciente na base de dados.
+     * Link: http://localhost:8080/api/paciente
+     *
+     * @param novoPaciente PacienteModel contendo os dados do novo paciente.
+     * @return pacienteDTO representando o paciente salvo.
+     */
+    @PostMapping()
+    public ResponseEntity<PacienteDTO> salvar(@Valid @RequestBody PacienteModel novoPaciente) {
+        PacienteDTO novoPacienteDTO = pacienteService.salvar(novoPaciente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoPacienteDTO);
+    }
+
+    /**
+     * Atualiza os dados de um paciente existente.
+     * Link: http://localhost:8080/api/paciente
+     *
+     * @param pacienteExistente pacienteModel contendo os dados atualizados do paciente.
+     * @return pacienteDTO representando o paciente atualizado.
+     * @link http://localhost:8080/api/paciente
+     */
+    @PutMapping
+    public ResponseEntity<PacienteDTO> atualizar(@Valid @RequestBody PacienteModel pacienteExistente) {
+        PacienteDTO pacienteExistenteDTO = pacienteService.atualizar(pacienteExistente);
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteExistenteDTO);
+    }
+
+    /**
+     * Deleta um paciente da base de dados.
+     * Link: http://localhost:8080/api/paciente
+     *
+     * @param pacienteExistente pacienteModel contendo os dados do paciente a ser deletado.
+     * @link http://localhost:8080/api/paciente
+     */
+    @DeleteMapping
+    public void deletar(@Valid @RequestBody PacienteModel pacienteExistente) {
+        pacienteService.deletar(pacienteExistente);
+    }
 }
