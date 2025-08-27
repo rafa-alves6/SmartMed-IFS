117959a7b4f4618bc2fa9bb4f279e159b8f0cb7a
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-08-27 17:15:11 -0300

    sem spring security

---
+++ src/main/java/br/com/smartmed/consultas/config/BcryptConfig.java
@@ -0,0 +1,14 @@
+package br.com.smartmed.consultas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BcryptConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
